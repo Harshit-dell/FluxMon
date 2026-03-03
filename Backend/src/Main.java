af689d2e240a0686b4df1a86f97031dabe74b893
Harshit-dell <harshitsahu967@gmail.com>
2026-03-03 05:44:30 +0530

    memory mapping

--- Backend/src/Main.java
+++ Backend/src/Main.java
@@ -17,6 +17,30 @@ public class Main {
         clearTreminalScreen();
         getStreamValue(TotalLines.lines);
     }
+    public static void getStreamValue(int totalLine){
+        try{
+            Files.list(Path.of("/proc"))
+                    .map(path->path.getFileName().toString())
+                    .filter(name->name.matches("\\d+"))
+                    .map(Integer::parseInt)
+                    .sorted()
+                    .forEach( pid ->{
+                        try{
+                            Path path=Path.of("/proc/" + pid + "/comm");
+                            String content=Files.readString(path).trim();
+                            System.out.print(pid+"->"+content+"--->");
+                            getMemoryMap(pid);
+                        }
+                        catch (Exception e){
+                            System.out.println(e.getMessage());
+                        }
+                    });
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
     public static  void clearTreminalScreen() throws InterruptedException, IOException {
     ProcessBuilder builder=new ProcessBuilder("bash","-c","clear");
     Process p=builder.start();
@@ -48,30 +72,21 @@ public class Main {
         }
         return  null;
     }
-    public static void getStreamValue(int totalLine){
+    public static void getMemoryMap(Integer pid) {
+        Path path = Path.of("/proc/" + pid + "/status");
+        try {
+            String content = Files.readString(path);
+            for (String line : content.split("\n")) {
+                String[] parts = line.split("\\s+");
+                if (parts[0].equals("VmRSS:")) {
+                    System.out.println(parts[1] + " kB");
+                    return;
+                }
+            }
+            System.out.println("Kernal Thread");
 
-    try{
-        Files.list(Path.of("/proc"))
-                .map(path->path.getFileName().toString())
-                .filter(name->name.matches("\\d+"))
-                .map(Integer::parseInt)
-                .sorted()
-                .limit(totalLine)
-                .forEach( pid ->{
-                    try{
-                        Path path=Path.of("/proc/" + pid + "/comm");
-                        String content=Files.readString(path);
-                        System.out.print(pid+"->"+content);
-
-                    }
-                    catch (Exception e){
-                        System.out.println(e.getMessage());
-                    }
-                });
-    }
-        catch (Exception e){
-        System.out.println(e.getMessage());
-    }
-
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
