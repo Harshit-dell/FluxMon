f7bda462040c4baf44587446e335ed43ae62d5ca
Harshit-dell <harshitsahu967@gmail.com>
2026-03-06 00:44:02 +0530

    Working prototype idea

--- Backend/src/Resources.java
+++ Backend/src/Resources.java
@@ -1,21 +1,31 @@
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Resources {
     public static void getStreamValue(int totalLine){
         try{
+            final int[] count={totalLine};
             Files.list(Path.of("/proc"))
                     .map(path->path.getFileName().toString())
                     .filter(name->name.matches("\\d+"))
                     .map(Integer::parseInt)
-                    .limit(totalLine)
+//                    .limit(totalLine)
                     .sorted()
                     .forEach( pid ->{
+                        if(count[0]<=0){
+                            return;
+                        }
                         try{
-                            Path path=Path.of("/proc/" + pid + "/comm");
-                            String content=Files.readString(path).trim();
-                            System.out.print(pid+"->"+content+"--->");
-                            Resources.getMemoryMap(pid);
+                           if(isKThread(pid) ){
+                               System.out.print("  "+pid+":  ");
+                               Resources.getMemoryMap(pid);
+                               Path path=Path.of("/proc/" + pid + "/comm");
+                               String content=Files.readString(path).trim();
+                               System.out.println(content);
+                               count[0]--;
+                           }
                         }
                         catch (Exception e){
                             System.out.println(e.getMessage());
@@ -26,6 +36,18 @@ public class Resources {
             System.out.println(e.getMessage());
         }
     }
+    public static boolean isKThread(int pid) throws IOException {
+        Path path = Path.of("/proc/" + pid + "/status");
+        String content = Files.readString(path);
+        for (String line : content.split("\n")) {
+            String[] parts = line.split("\\s+");
+            if (parts[0].equals("VmRSS:")) {
+                return true;
+            }
+        }
+        return false;
+        //here optimization is possible but will leave it for later
+    }
     public static void getMemoryMap(Integer pid) {
         Path path = Path.of("/proc/" + pid + "/status");
         try {
@@ -33,11 +55,12 @@ public class Resources {
             for (String line : content.split("\n")) {
                 String[] parts = line.split("\\s+");
                 if (parts[0].equals("VmRSS:")) {
-                    System.out.println(parts[1] + " kB");
+                    int memoryInGb=(Integer.parseInt(parts[1])/1024) ;
+                    System.out.print(memoryInGb + " MB ");
                     return;
                 }
             }
-            System.out.println("Kernal Thread");
+            System.out.print("KThread   ");
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
